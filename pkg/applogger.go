5897c56712ad57d31bfd197069608f462140c35f
eshu0 <willsawesometoolkit@gmail.com>
2020-11-27 05:28:20 +0000

    Update applogger.go

--- pkg/applogger.go
+++ pkg/applogger.go
@@ -52,7 +52,7 @@ func (al *AppLogger) LogDebug(cmd string, data ...interface{}) {
 		al.StartLogging()
 	}
 	if al.Log != nil {
-		al.Log.LogDebug(cmd, data)
+		al.Log.LogDebug(cmd, data...)
 	}
 }
 
@@ -61,7 +61,7 @@ func (al *AppLogger) LogWarn(cmd string, data ...interface{}) {
 		al.StartLogging()
 	}
 	if al.Log != nil {
-		al.Log.LogWarn(cmd, data)
+		al.Log.LogWarn(cmd, data...)
 	}
 }
 
@@ -70,7 +70,7 @@ func (al *AppLogger) LogInfo(cmd string, data ...interface{}) {
 		al.StartLogging()
 	}
 	if al.Log != nil {
-		al.Log.LogInfo(cmd, data)
+		al.Log.LogInfo(cmd, data...)
 	}
 }
 
@@ -79,7 +79,7 @@ func (al *AppLogger) LogError(cmd string, data ...interface{}) {
 		al.StartLogging()
 	}
 	if al.Log != nil {
-		al.Log.LogError(cmd, data)
+		al.Log.LogError(cmd, data...)
 	}
 }
 
@@ -109,7 +109,7 @@ func (al *AppLogger) LogDebugf(cmd string, msg string, data ...interface{}) {
 		al.StartLogging()
 	}
 	if al.Log != nil {
-		al.Log.LogDebugf(cmd, msg, data)
+		al.Log.LogDebugf(cmd, msg, data...)
 	}
 }
 
@@ -118,7 +118,7 @@ func (al *AppLogger) LogWarnf(cmd string, msg string, data ...interface{}) {
 		al.StartLogging()
 	}
 	if al.Log != nil {
-		al.Log.LogWarnf(cmd, msg, data)
+		al.Log.LogWarnf(cmd, msg, data...)
 	}
 }
 
@@ -127,7 +127,7 @@ func (al *AppLogger) LogInfof(cmd string, msg string, data ...interface{}) {
 		al.StartLogging()
 	}
 	if al.Log != nil {
-		al.Log.LogInfof(cmd, msg, data)
+		al.Log.LogInfof(cmd, msg, data...)
 	}
 }
 
@@ -136,6 +136,6 @@ func (al *AppLogger) LogErrorf(cmd string, msg string, data ...interface{}) {
 		al.StartLogging()
 	}
 	if al.Log != nil {
-		al.Log.LogErrorf(cmd, msg, data)
+		al.Log.LogErrorf(cmd, msg, data...)
 	}
 }
