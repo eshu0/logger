b7e235d721daa357e3f90926f4f93647bf4a6874
eshu0 <willsawesometoolkit@gmail.com>
2021-08-21 07:44:43 +0100

    Updated interfaces

--- pkg/interfaces/isimplelogger.go
+++ pkg/interfaces/isimplelogger.go
@@ -25,7 +25,7 @@ type ISimpleLogger interface {
 	LogDebugf(cmd string, message string, data ...interface{})
 
 	LogError(cmd string, data ...interface{})
-	LogErrorE(cmd string, e error)
+	LogErrorE(cmd string, data error)
 	LogErrorEf(cmd string, message string, e error)
 	LogWarn(cmd string, data ...interface{})
 	LogInfo(cmd string, data ...interface{})
