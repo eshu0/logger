8056589b5f8bd630d63034b8143ec2bce75ff04b
eshu0 <willsawesometoolkit@gmail.com>
2021-11-28 10:16:13 +0000

    added test

---
+++ pkg/simplelogger_test.go
@@ -0,0 +1,32 @@
+package simplelogger
+
+import (
+	"testing"
+)
+
+func TestLoadConfig(t *testing.T) {
+
+	log := NewSimpleLogger("slogger.log", "123")
+
+	// lets open a file log using the session
+	openerrors := log.OpenAllChannels()
+
+	if len(openerrors) > 0 {
+
+		t.Errorf("Open failed with %d errors", len(openerrors))
+		for i := 0; i < len(openerrors); i++ {
+			t.Error(openerrors[i])
+		}
+	}
+
+	closeerrors := log.CloseAllChannels()
+
+	if len(closeerrors) > 0 {
+
+		t.Errorf("Close failed with %d errors", len(closeerrors))
+		for i := 0; i < len(closeerrors); i++ {
+			t.Error(closeerrors[i])
+		}
+	}
+
+}
